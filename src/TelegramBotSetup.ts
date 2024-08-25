abe3f08284a2b25787107fc35939f7e800d41475
Lukas <lovretic2002@gmail.com>
2024-08-25 19:23:47 +0200

    POST ruta, vjv radi sve iz prve jer sam najjaci

--- src/TelegramBotSetup.ts
+++ src/TelegramBotSetup.ts
@@ -5,6 +5,9 @@ config(); // Load environment variables from .env file
 
 const token = process.env.TELEGRAM_TOKEN as string;
 
+const usersThatReciveErrorReports =
+  process.env.USERS_THAT_RECIVE_ERROR_REPORTS?.split(",").filter(Boolean) || [];
+
 // Create a bot that uses 'polling' to fetch new updates
 export const bot = new TelegramBot(token, { polling: true });
 
@@ -20,4 +23,16 @@ export const sendTelegramMessage: (
   options?: SendMessageArguments[2]
 ) => Promise<void> = async (chatId, text, options) => {
   await bot.sendMessage(chatId, text, options);
+};
+
+// Jel ovo sranje smiješno?
+export const sendToSubscribedUsers = async (text: SendMessageArguments[1]) => {
+  try {
+    for (const user of usersThatReciveErrorReports) {
+      await sendTelegramMessage(user, text);
+    }
+  } catch (error) {
+    console.error("Error sending message to subscribed users", error);
+    sendToSubscribedUsers(text);
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import Express from "express";
-import { bot } from "./TelegramBotSetup";
+import { bot, sendToSubscribedUsers } from "./TelegramBotSetup";
 import { OnTelegramMessageHandler } from "./OnTelegramMessageHandler";
 // Create a new express application instance
 const app = Express();
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on("message", OnTelegramMessageHandler);
-
-// bot.sendMessage("7198186743", "Di si rista!");
-
-/*
-  Start the Express server
-*/
-
 // The port the express app will listen on
 const port = process.env.PORT || 3000;
+
+// Listen for any kind of message.
+bot.on("message", OnTelegramMessageHandler);
+
+app.post("/report_error", (req, res) => {
+  const sentErrorReport = req.body;
+
+  console.log(sentErrorReport);
+
+  const stringifiedError = JSON.stringify(sentErrorReport).slice(0, 4096); // Max message length
+
+  sendToSubscribedUsers(stringifiedError);
+
+  res.status(200).send();
+});
 
 // Start the Express server
 app.listen(port, () => {
