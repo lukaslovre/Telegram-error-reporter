299ea3f1e2f29d438b39441ab9ffc0c9f4835bd6
Lukas Lovretić <llovretic@fina.hr>
2024-08-26 12:26:45 +0200

    .env.example, BUGFIX: dodan body-parser da moze gledati req.body

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { bot, sendToSubscribedUsers } from "./TelegramBotSetup.js";
 import { OnTelegramMessageHandler } from "./OnTelegramMessageHandler.js";
 // Create a new express application instance
 const app = Express();
-// The port the express app will listen on
-const port = process.env.PORT || 3000;
+
+app.use(Express.json());
 
 // Listen for any kind of message.
 bot.on("message", OnTelegramMessageHandler);
@@ -14,12 +14,22 @@ app.post("/report_error", (req, res) => {
 
   console.log(sentErrorReport);
 
-  const stringifiedError = JSON.stringify(sentErrorReport).slice(0, 4096); // Max message length
+  if (!sentErrorReport) {
+    return res.status(400).send("Request needs to be a valid JSON.");
+  }
+
+  const stringifiedError = JSON.stringify(sentErrorReport, null, 2).slice(
+    0,
+    4096
+  ); // Max message length
 
   sendToSubscribedUsers(stringifiedError);
 
   res.status(200).send();
 });
+
+// The port the express app will listen on
+const port = process.env.PORT || 3000;
 
 // Start the Express server
 app.listen(port, () => {
